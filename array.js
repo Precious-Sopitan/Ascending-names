6231a6366435b6eb9a6427f72357fecedc8efddd
Sopitan Precious Abolade <125654024+Precious-Sopitan@users.noreply.github.com>
2023-09-08 20:22:15 +0100

    Add files via upload

---
+++ array.js
@@ -0,0 +1,5 @@
+let nameOfAttendance = ["Bola", "Susan", "Jayden", "Daylan", "Promise", "Abolade", "Ademurewa"]
+
+let names = nameOfAttendance.sort();
+ 
+document.getElementById("demo").innerHTML = names
